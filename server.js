aff9ec22a9b0739c02bf65dffd613968ee4d6d15
Kees Jansen <jansengsy@gmail.com>
2023-10-19 14:50:02 +0100

    setup the socket io server and implemented a sample html file to connect to the server - provided by socketio tutorial

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
 
+const express = require('express');
+const { Server } = require('socket.io');
 const path = require('path');
 const http = require('http');
 
-const express = require('express');
-const socketio = require('socket.io');
-
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server)
+
+// I can use this to serve a directory to the browser. 
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   console.log(`Server started. Listening on port ${port}`);
 });
