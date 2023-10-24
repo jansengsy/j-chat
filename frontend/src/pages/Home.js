dcb0c49c735e7a4bd6c26e058e1e0eaac8768f92
Kees Jansen <jansengsy@gmail.com>
2023-10-24 15:30:33 +0100

    added temporary outlet in home component to render children

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function Home() {
   return (
-    <h1>Home!</h1>
+    <div>
+      <h1>Home!</h1>
+      <Outlet />
+    </div>
   )
 }
