ba8a897007f65d3dc54ecb3a93babf851a881db9
Kees Jansen <jansengsy@gmail.com>
2023-10-24 17:18:41 +0100

    added a temporary logout button to the home component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,21 @@
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 
+import { AuthContext } from "../context/authContext";
+
 export default function Home() {
+
+  const { deleteToken } = useContext(AuthContext);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    deleteToken('token');
+  }
+
   return (
     <div>
       <h1>Home!</h1>
+      <button onClick={handleLogout}>Logout!</button>
       <Outlet />
     </div>
   )
