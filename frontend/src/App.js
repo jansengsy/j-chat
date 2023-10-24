7f42421f081c051f3e4dd5b8eaf50ba007fb9690
Kees Jansen <jansengsy@gmail.com>
2023-10-24 17:16:13 +0100

    tidied up app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,7 @@
-import { useContext, useEffect } from "react";
-
-// Context
-import { AuthContext } from "./context/authContext";
-
-// Hooks
-import useAuth from "./hooks/useAuth";
-
-// Routing
 import { RouterProvider } from "react-router-dom";
 import Router from "./routing/router";
 
 export default function App() {
-
-  const {token} = useContext(AuthContext);
 
   return (
     <RouterProvider router={Router}/>
