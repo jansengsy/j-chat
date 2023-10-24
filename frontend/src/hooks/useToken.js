611dd934429a1ecebc23de94df7230e43fa649e4
Kees Jansen <jansengsy@gmail.com>
2023-10-24 14:59:10 +0100

    updated use token to return the whole object response from the server not just the jwy - I might change this back just working things out atm

--- frontend/src/hooks/useToken.js
+++ frontend/src/hooks/useToken.js
@@ -6,7 +6,7 @@ export default function useToken() {
   const getToken = () => {
     const tokenString = localStorage.getItem('token');
     const userToken = JSON.parse(tokenString);
-    return userToken?.token;
+    return userToken;
   }
 
   const [token, setToken] = useState(getToken());
@@ -14,7 +14,7 @@ export default function useToken() {
   // Set the token into storage
   const saveToken = (userToken) => {
     localStorage.setItem('token', JSON.stringify(userToken));
-    setToken(userToken.token);
+    setToken(userToken);
   }
 
   // Delete the token from storage
