6d92444b2b412846b23d0908201e67e19fa15e8a
Kees Jansen <jansengsy@gmail.com>
2023-10-26 16:26:28 +0100

    updated nav component to display current room and have menu items clickable so they connect to a specific room

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -1,27 +1,36 @@
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
+import { RoomContext } from "../../context/roomContext";
+import { joinRoom } from "../../socket";
 
 import '../../styles/nav.css'
 
 export default function Nav() {
 
   const { deleteToken } = useContext(AuthContext);
+  const { room, setRoom } = useContext(RoomContext);
 
   const handleLogout = (e) => {
     e.preventDefault();
     deleteToken('token');
   }
 
+  const handleJoinRoom = async (roomID) => {
+    joinRoom(roomID);
+    setRoom(roomID);
+  }
+
   return (
     <div className="sidenav">
       <div className="header">
         <h2>J-Chat</h2>
+        <h2>{`Current room: ${room}`}</h2>
       </div>
       <div className="menu">
         <ul>
-          <li>Chats</li>
-          <li>Contacts</li>
-          <li>Settings</li>
+          <li onClick={() => handleJoinRoom(1)}>Room 1</li>
+          <li onClick={() => handleJoinRoom(2)}>Room 2</li>
+          <li onClick={() => handleJoinRoom(3)}>Room 3</li>
         </ul>
       </div>
       <button onClick={handleLogout} className='logout-button'>Logout</button>
