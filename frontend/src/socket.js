28df45438bff25f6a6dbad0212bce817f16d2374
Kees Jansen <jansengsy@gmail.com>
2023-10-26 16:21:52 +0100

    added join room and chatToRoom methods to be used in application

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,4 +3,15 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
-export const socket = io(URL);
+// Creating the socket for the client
+const socket = io(URL);
+
+export const joinRoom = (roomID) => {
+  socket.emit('join-room', roomID);
+}
+
+export const chatToRoom = (data) => {
+  socket.emit('chat-to-room', data);
+}
+
+export default socket;
