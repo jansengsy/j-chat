16857e5a58c8e4e5d0a224f9e1c5766930f93dfe
Kees Jansen <jansengsy@gmail.com>
2023-10-26 22:09:13 +0100

    tidied up home component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,6 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
-import { AuthContext } from "../context/authContext";
 import { RoomContextProvider } from "../context/roomContext";
 
 import socket from '../socket';
@@ -9,8 +8,6 @@ import socket from '../socket';
 import Nav from "../components/nav/Nav";
 
 export default function Home() {
-
-  const { deleteToken } = useContext(AuthContext);
 
   useEffect(() => {
     socket.connect();
