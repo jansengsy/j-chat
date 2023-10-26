736a669d738e33367c70524408247803c4d27f02
Kees Jansen <jansengsy@gmail.com>
2023-10-26 17:14:56 +0100

    removed strict mode as it was triggering double message sending

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,7 @@ import App from './App';
 import { AuthContextProvider } from './context/authContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
