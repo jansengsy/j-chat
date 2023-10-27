479375e1a6dbdf790b4ef32f22bbc0055f6ef620
Kees Jansen <jansengsy@gmail.com>
2023-10-27 11:18:05 +0100

    updated some styles on the body and chat page to fix minor ui issues

--- frontend/src/components/chat/MessageContainer.js
+++ frontend/src/components/chat/MessageContainer.js
@@ -1,7 +1,7 @@
 export default function MessageContainer({messages}) {
   return (
     <div className='message-container'>
-      <ul>
+      <ul className='message-list'>
         {messages.map((message, index) => (
           <li className='message' key={index}>
             {`${message.user_id} wrote: `} <strong>{`${message.content} `}</strong>, {`at: ${message.timestamp}`}
