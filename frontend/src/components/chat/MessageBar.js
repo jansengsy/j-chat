f1ca198369bdb279f3445fffdc9f4291738ca6ec
Kees Jansen <jansengsy@gmail.com>
2023-10-27 16:02:39 +0100

    added error handling for empty messages client side

--- frontend/src/components/chat/MessageBar.js
+++ frontend/src/components/chat/MessageBar.js
@@ -5,9 +5,19 @@ import { AuthContext } from "../../context/authContext";
 export default function MessageBar({room}) {
 
   const { user } = useContext(AuthContext);
+  const [error, setError] = useState('');
   const [inputValue, setInputValue] = useState('');
 
   const handleSendMessage = () => {
+    setError('');
+    if (inputValue === '') {
+      setError('Messages can\'t be empty!');
+      document.getElementById("message-input").classList.add("shake");
+      setTimeout(() => {
+        document.getElementById("message-input").classList.remove("shake");
+      }, 400);
+      return;
+    }
     chatToRoom({ room: room, message: inputValue, user_id: user._id });
     setInputValue('');
   };
@@ -15,10 +25,12 @@ export default function MessageBar({room}) {
   return (
     <div className="message-bar">
       <input
+        id="message-input"
         className="message-input"
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        placeholder={error}
         />
       <button
         className="message-button"
