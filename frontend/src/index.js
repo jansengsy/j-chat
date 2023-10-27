f66e63340230405f60c15880351cc9f5da1fd4c7
Kees Jansen <jansengsy@gmail.com>
2023-10-27 10:43:29 +0100

    added font awesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthContextProvider } from './context/authContext';
 
 import './styles/index.css';
 
-import App from './App';
+// Icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-import { AuthContextProvider } from './context/authContext';
+library.add(fas, far, fab);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
