bc86d7541c0043b1f9608dba49d3e648a902e41a
Kees Jansen <jansengsy@gmail.com>
2023-10-29 12:50:31 +0000

    user model now converts email and username to lowercase

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, lowercase: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   verified: { type: Boolean, default: false },
   token: { type: String },
