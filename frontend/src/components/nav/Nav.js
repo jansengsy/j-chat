bf806f1d730dbdd4d0e19cc0b011e627ef9fb3fd
Kees Jansen <jansengsy@gmail.com>
2023-10-30 14:02:39 +0000

    updated some stylings on nav and message bar

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -29,9 +29,42 @@ export default function Nav() {
       </div>
       <div className="nav-menu">
         <ul>
-          <li className='nav-item' onClick={() => handleJoinRoom(1)}>Room 1</li>
-          <li className='nav-item' onClick={() => handleJoinRoom(2)}>Room 2</li>
-          <li className='nav-item' onClick={() => handleJoinRoom(3)}>Room 3</li>
+          <li
+            className={`nav-item ${ room === 1 ? 'selected-nav-item' : ''}`}
+            onClick={() => handleJoinRoom(1)}
+          >
+            Room 1
+          </li>
+          <li
+            className={`nav-item ${ room === 2 ? 'selected-nav-item' : ''}`}
+            onClick={() => handleJoinRoom(2)}
+          >
+            Room 2
+          </li>
+          <li
+            className={`nav-item ${ room === 3 ? 'selected-nav-item' : ''}`}
+            onClick={() => handleJoinRoom(3)}
+          >
+            Room 3
+          </li>
+          <li
+            className={`nav-item ${ room === 4 ? 'selected-nav-item' : ''}`}
+            onClick={() => handleJoinRoom(4)}
+          >
+            Room 4
+          </li>
+          <li
+            className={`nav-item ${ room === 5 ? 'selected-nav-item' : ''}`}
+            onClick={() => handleJoinRoom(5)}
+          >
+            Room 5
+          </li>
+          <li
+            className={`nav-item ${ room === 6 ? 'selected-nav-item' : ''}`}
+            onClick={() => handleJoinRoom(6)}
+          >
+            Room 6
+          </li>
         </ul>
       </div>
       <button onClick={handleLogout} className='logout-button'>Logout</button>
