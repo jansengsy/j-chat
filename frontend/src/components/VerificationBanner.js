c40c4c7a30cdcaf8741baf6a055cd33ce1c1f1da
Kees Jansen <jansengsy@gmail.com>
2023-10-30 10:58:58 +0000

    added the verification banner which now allows users to resend the verification email and will only display if a user hasn't verified their email

---
+++ frontend/src/components/VerificationBanner.js
@@ -0,0 +1,44 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+
+import { AuthContext } from "../context/authContext";
+
+export default function VerificationBanner() {
+
+  const {user, token} = useContext(AuthContext);
+  const [emailSent, setEmailSent] = useState(false);
+
+  const sendVerificationEmail = async () => {
+    try {
+      await axios.post('http://localhost:3000/email/sendVerificationEmail',
+        {
+          verification_token: user.verification_token,
+          username: user.username,
+          email: user.email
+        }, 
+        {
+          headers: {
+              'Authorization': `Bearer ${token}`,
+          }
+        }
+      );
+
+      setEmailSent(true);
+    } catch (err) {
+      setEmailSent(false);
+    }
+  };
+
+  return (
+    <div className="banner-container">
+      <div className={ emailSent ? 'banner-success' : 'banner' }>
+        { emailSent ? 
+            <p>Email has been sent! Please check your inbox.</p> : 
+            <p>Please verify your Email. Need another email link?
+              <button className="banner-link" onClick={sendVerificationEmail}>Click here!</button>
+            </p> 
+        }
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
 
 import { RoomContextProvider } from "../context/roomContext";
+import { AuthContext } from "../context/authContext";
 
 import socket from '../socket';
 
 import Nav from "../components/nav/Nav";
+import VerificationBanner from "../components/VerificationBanner";
 
 export default function Home() {
+
+  const {user} = useContext(AuthContext);
 
   useEffect(() => {
     socket.connect();
@@ -15,6 +19,7 @@ export default function Home() {
 
   return (
     <>
+      { user.verified === false ? <VerificationBanner /> : <></> }
       <RoomContextProvider>
         <Nav />
         <Outlet />
